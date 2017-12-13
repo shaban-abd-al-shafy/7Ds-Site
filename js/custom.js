519bb0435af04eea00af5880e836cfe29a3c6882
shaban-abd-al-shafy <agriengshaban@gmail.com>
2017-12-13 03:43:34 -0800

    second commitمش

--- js/custom.js
+++ js/custom.js
@@ -159,4 +159,63 @@ scrollButton.click(function () {
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
   ga('create', 'UA-59016839-1', 'auto');
   ga('send', 'pageview');
+/* end header for video */
 
+/*start product section*/
+
+    var articles = $('article > .item-wrapper'),
+    lightingRgb = '255,255,255';
+
+    articles.mousemove(function(e) {
+      var current = $(this),
+          x = current.width() - e.offsetX * 2,
+          y = current.height() - e.offsetY * 2,
+          rx = -x / 30,
+          ry = y / 24,
+          deg = Math.atan2(y, x) * (180 / Math.PI) + 45;
+      current.css({"transform":"scale(1.05) rotateY("+rx+"deg) rotateX("+ry+"deg)"});
+      $('figure > .lighting',this).css('background','linear-gradient('+deg+'deg, rgba('+lightingRgb+',0.32) 0%, rgba('+lightingRgb+',0) 100%)');
+    });
+
+    articles.on({
+      'mouseenter':function() {
+        var current = $(this);
+        current.addClass('enter ease').removeClass("leave");
+        setTimeout(function(){
+          current.removeClass('ease');
+        }, 280);
+      },
+      'mouseleave':function() {
+        var current = $(this);
+        current.css({"transform":"rotate(0)"});
+        current.removeClass('enter').addClass("leave");
+        $('figure > .lighting',this).removeAttr('style');
+      }}
+    );
+
+/*end product section*/
+
+/* strat clients section */
+$(document).ready(function(){
+    $('.customer-logos').slick({
+        slidesToShow: 7,
+        slidesToScroll: 2,
+        autoplay: true,
+        autoplaySpeed: 1200,
+        arrows: false,
+        dots: false,
+            pauseOnHover: false,
+            responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 3
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 2
+            }
+        }]
+    });
+});
+/* end clients section */
